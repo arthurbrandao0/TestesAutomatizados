7baded5cd3a275ddc0faaa20920407e9ad25235e
arthurbrandao0 <arthurbm15@gmail.com>
2017-10-20 13:36:04 -0200

    TC 6392 finalizado

--- TestesAutomatizados/Cobrança e Boleto/6392_SimulacaoDeGeracaoDeCobrancaSemConfirmacao.cs
+++ TestesAutomatizados/Cobrança e Boleto/6392_SimulacaoDeGeracaoDeCobrancaSemConfirmacao.cs
@@ -8,7 +8,7 @@ using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UITest.Extension;
 using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
-
+using OpenQA.Selenium.Remote;
 
 namespace TestesAutomatizados.Cobrança_e_Boleto
 {
@@ -25,10 +25,17 @@ namespace TestesAutomatizados.Cobrança_e_Boleto
         [TestMethod]
         public void SimulacaoDeGeracaoDeCobrancaSemConfirmacao6392Metodo()
         {
-            // Para gerar código para este teste, selecione "Gerar Código para Teste de Interface do Usuário Codificado" no menu de atalho e selecione um dos itens do menu.
-            this.UIMap.AbrirSimulacaoDeCobranca();
-            this.UIMap.SelecionarTodasCobrancasEmSimulacaoCobranca();
-            this.UIMap.InformarQuantidadeMeses1();
+            MultiClubesFunctions mcFunctions = new MultiClubesFunctions();
+            MultiClubesMenus McMenus = new MultiClubesMenus();
+
+            var dc = new DesiredCapabilities();
+            dc.SetCapability("app", @"\\tsidev\Triade\Application\Dev\MultiClubes\System\MultiClubes\MultiClubes.UI.application");
+            dc.SetCapability("debugConnectToRunningApp", true);
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
+
+            McMenus.AcessarMenuOperacaoFinanceiroCobrancaSimulacaoDeCobranca();
+            //this.UIMap.SelecionarTodasCobrancasEmSimulacaoCobranca();
+            //this.UIMap.InformarQuantidadeMeses1();
             this.UIMap.ClicarBotaoSimularGeracaoCobrancaEDepoisClicarNao();
             this.UIMap.VerificarTituloSimulacaoDeCobranca();
             this.UIMap.VerificarSeBotaoFecharExiste();
